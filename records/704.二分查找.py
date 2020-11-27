13b3014975af8b789be1b7ab10372b5e25ebdd8b
Z <liuzhi0818@gmail.com>
2020-11-27 20:18:33 +0800

    704.二分查找

---
+++ records/704.二分查找.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=704 lang=python3
+#
+# [704] 二分查找
+#
+
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] == target:
+                return mid
+        return -1
+
+
+# @lc code=end
