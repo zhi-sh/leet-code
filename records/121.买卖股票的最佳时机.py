ad76dd40c209bcb8e5fb6bc3e0aed8eb0adbcb78
Z <liuzhi0818@gmail.com>
2020-12-03 22:53:44 +0800

    121.买卖股票的最佳时机

--- records/121.买卖股票的最佳时机.py
+++ records/121.买卖股票的最佳时机.py
@@ -8,12 +8,17 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
         sz = len(prices)
         dp = [[0] * 2 for _ in range(sz)]
         for i in range(sz):
             if i - 1 == -1:
-                dp[i][0] = 0  # dp[i][0] = max(dp[-1][0], d[-1][1]+price[i]) = max(0, -inf+price[i])
-                dp[i][1] = -prices[i]  # dp[i][1] = max(dp[i-1][1], dp[i-1][0]-price[i]) = max(-inf, 0-prices[i])
+                dp[i][
+                    0] = 0  # dp[i][0] = max(dp[-1][0], d[-1][1]+price[i]) = max(0, -inf+price[i])
+                dp[i][1] = -prices[
+                    i]  # dp[i][1] = max(dp[i-1][1], dp[i-1][0]-price[i]) = max(-inf, 0-prices[i])
                 continue
             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
             dp[i][1] = max(dp[i - 1][1], -prices[i])
