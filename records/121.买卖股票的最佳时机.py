f122a062ca4e39f70097e5cf6b27847e5dae346d
Z <liuzhi0818@gmail.com>
2020-12-03 22:54:28 +0800

    121.买卖股票的最佳时机

--- records/121.买卖股票的最佳时机.py
+++ records/121.买卖股票的最佳时机.py
@@ -15,10 +15,8 @@ class Solution:
         dp = [[0] * 2 for _ in range(sz)]
         for i in range(sz):
             if i - 1 == -1:
-                dp[i][
-                    0] = 0  # dp[i][0] = max(dp[-1][0], d[-1][1]+price[i]) = max(0, -inf+price[i])
-                dp[i][1] = -prices[
-                    i]  # dp[i][1] = max(dp[i-1][1], dp[i-1][0]-price[i]) = max(-inf, 0-prices[i])
+                dp[i][0] = 0  # dp[i][0] = max(dp[-1][0], d[-1][1]+price[i]) = max(0, -inf+price[i])
+                dp[i][1] = -prices[i]  # dp[i][1] = max(dp[i-1][1], dp[i-1][0]-price[i]) = max(-inf, 0-prices[i])
                 continue
             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
             dp[i][1] = max(dp[i - 1][1], -prices[i])
