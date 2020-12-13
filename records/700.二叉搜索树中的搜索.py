54ee122da223c035a729f671f20ca515c8186d9c
Z <liuzhi0818@gmail.com>
2020-12-13 10:36:10 +0800

    700.二叉搜索树中的搜索

---
+++ records/700.二叉搜索树中的搜索.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=700 lang=python3
+#
+# [700] 二叉搜索树中的搜索
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None:
+            return None 
+        if val == root.val:
+            return root 
+        elif val < root.val:
+            return self.searchBST(root.left, val)
+        elif val > root.val:
+            return self.searchBST(root.right, val)
+# @lc code=end
+
