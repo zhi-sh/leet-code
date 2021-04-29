5e4d5a75a8dc0468a7aa751c56d4301668d95269
Z <liuzhi0818@gmail.com>
2021-04-29 22:37:07 +0800

    爬楼梯

---
+++ records/70.爬楼梯.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=70 lang=python3
+#
+# [70] 爬楼梯
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2 
+
+        dp = [0] * (n+1)
+        dp[1] = 1 
+        dp[2] = 2 
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+# @lc code=end
+
