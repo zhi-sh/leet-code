ed32f568d827151938db50e6ebb25f1b70f9b25f
Z <liuzhi0818@gmail.com>
2021-04-29 22:35:19 +0800

    钱兑换-ii

---
+++ records/518.零钱兑换-ii.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=518 lang=python3
+#
+# [518] 零钱兑换 II
+#
+
+# @lc code=start
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0]*(amount+1)
+        dp[0] = 1
+        
+        for i in range(len(coins)):
+            for j in range(coins[i], amount+1):
+                if j-coins[i]>=0:
+                    dp[j] = dp[j] + dp[j-coins[i]]
+
+        return dp[amount]
+# @lc code=end
+
